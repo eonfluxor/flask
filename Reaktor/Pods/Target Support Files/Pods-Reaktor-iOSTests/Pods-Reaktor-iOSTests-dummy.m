332259d1ab63014e1616b4d88dcb1777deaaea0e
hassan uriostegui <hassanvfx@gmail.com>
2018-09-10 16:58:33 -0700

    fixed podfile

---
+++ Reaktor/Pods/Target Support Files/Pods-Reaktor-iOSTests/Pods-Reaktor-iOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Reaktor_iOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_Reaktor_iOSTests
+@end
---
+++ Reaktor/Pods/Target Support Files/Pods-Reaktor-tvOSTests/Pods-Reaktor-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Reaktor_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_Reaktor_tvOSTests
+@end
