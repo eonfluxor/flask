a1a9acc67b6169e57c58578d932f7f06022f34db
hassan uriostegui <hassanvfx@gmail.com>
2018-09-10 15:13:15 -0700

    updated conditional import

--- Reaktor/Sources/Reaktor.swift
+++ Reaktor/Sources/Reaktor.swift
@@ -6,7 +6,11 @@
 //  Copyright © 2018 eonflux. All rights reserved.
 //
 
+#if os(iOS) || os(tvOS) || os(watchOS)
 import UIKit
+#elseif os(OSX) || os(macOS)
+import Cocoa
+#endif
 
 class Reaktor: NSObject {
 
