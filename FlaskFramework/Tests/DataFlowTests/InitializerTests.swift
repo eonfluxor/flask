09c030450d8963d177bed54a2a1c3fc8d3cb219f
hassan uriostegui <hassanvfx@gmail.com>
2018-09-11 21:19:33 -0700

    wip

---
+++ FlaskFramework/Tests/DataFlowTests/InitializerTests.swift
@@ -0,0 +1,27 @@
+//
+//  InitializerTests.swift
+//  Flask-iOS
+//
+//  Created by hassan uriostegui on 9/11/18.
+//  Copyright © 2018 eonflux. All rights reserved.
+//
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+import UIKit
+#elseif os(OSX) || os(macOS)
+import Cocoa
+#endif
+
+import XCTest
+
+class InitializerTests: XCTestCase {
+    class ViewController: UIViewController, FlaskOwner {
+        
+    }
+    
+    func testOwnerInit(){
+        let vc = ViewController()
+        
+    }
+    
+}
